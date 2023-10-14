9bfaead44f3426e1ab6c63ec4a767fb4fd969f45
torcuata22 <marilynjmarquez@gmail.com>
2023-10-14 16:58:13 -0400

    login and log out are working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id #this will allow logged in users to authenticate once and remian signed in for future requests
       flash[:notice] = "Log in successful"
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:notice] = "Logged out"
-    redirect_to root_path
+    redirect_to new_session_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :chatroom
   resources :sessions
   root "chatroom#index"
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: :destroy_session
   # You can add other custom routes as needed
 end
