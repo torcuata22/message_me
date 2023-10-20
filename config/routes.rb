81f788bfae0da78de647ee441571ac38b35898ef
torcuata22 <marilynjmarquez@gmail.com>
2023-10-20 14:27:52 -0400

    starting messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-
+  
   root "chatroom#index"
   resources :users
   resources :chatroom
   resources :sessions
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: :destroy_session
+  post 'message', to: 'messages#create'
   
 end
